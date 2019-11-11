31fae2b61284f803f132bb1b88c4ab8f3c8d96f3
liadove <alinadavletshina97@gmail.com>
2019-11-11 14:38:41 -0500

    changed the syntax and added default in case if the typed command is wrong

--- bash.js
+++ bash.js
@@ -2,18 +2,22 @@ const pwd = require('./pwd')
 const ls = require('./ls')
 
 
-process.stdin.on('data', (data) => {
+process.stdout.write("prompt >")
 
-  process.stdout.write("prompt >")
+process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
   switch (cmd) {
     case 'pwd':
-      pwd()
+      pwd();
+      process.stdout.write("\nprompt > ")
       break;
     case 'ls':
-      ls()
+      ls();
       break;
+      default:
+        process.stdout.write(cmd +" not a command")
+        process.stdout.write("\nprompt > ")
   }
-  process.stdout.write("\n prompt > ")
-}
+
+})
 
--- ls.js
+++ ls.js
@@ -1,22 +1,5 @@
 const fs = require('fs');
 
-// module.exports = () => {
-
-//   process.stdin.on('data', (data) => {
-//     const cmd = data.toString().trim();
-
-//     if (cmd === 'ls') {
-//       fs.readdir('./','utf8', (err, files) => {
-//         if (err) {
-//           throw err
-//         } else {
-//           process.stdout.write(files.join('\n'))
-//         }
-//       })
-//     }
-//   })
-// }
-
 module.exports = () => {
   fs.readdir('./','utf8', (err, files) => {
     if (err) {
@@ -24,5 +7,7 @@ module.exports = () => {
     } else {
       process.stdout.write(files.join('\n'))
     }
+
+    process.stdout.write("\nprompt > ")
   })
 }
